c806b2d05bfd410413fc03ecc977010d94be177f
Programming Hero <programming.hero1@gmail.com>
2025-10-30 19:24:16 -0500

    added bid delete api

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ async function run() {
         app.get('/bids', async (req, res) => {
 
             const query = {};
-            if(query.email){
+            if (query.email) {
                 query.buyer_email = email;
             }
 
@@ -143,6 +143,13 @@ async function run() {
         app.post('/bids', async (req, res) => {
             const newBid = req.body;
             const result = await bidsCollection.insertOne(newBid);
+            res.send(result);
+        })
+
+        app.delete('/bids/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bidsCollection.deleteOne(query);
             res.send(result);
         })
 
